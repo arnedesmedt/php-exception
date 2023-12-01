e1b58872841265ce2e5b2bb1d72f4b9de2aabf8b
Arne De Smedt <arnedesmdt@gmail.com>
2023-12-01 10:28:28 +0100

    Build property type map for exceptions

--- src/DefaultJsonSchemaException.php
+++ src/DefaultJsonSchemaException.php
@@ -6,13 +6,27 @@ namespace ADS\Exception;
 
 use ADS\JsonImmutableObjects\JsonSchemaAwareRecordLogic;
 use ADS\Util\StringUtil;
+use ReflectionClass;
 
+use function array_diff_key;
+use function array_flip;
 use function sprintf;
 use function strrchr;
 
 trait DefaultJsonSchemaException
 {
     use JsonSchemaAwareRecordLogic;
+
+    private const PROPERTIES_TO_IGNORE = [
+        'message',
+        'code',
+        'file',
+        'line',
+        '__useMaxValues',
+        '__propTypeMap',
+        '__schema',
+        '__arrayPropItemTypeMap',
+    ];
 
     protected string $type;
     protected string $detail;
@@ -35,6 +49,28 @@ trait DefaultJsonSchemaException
                 StringUtil::decamelize(strrchr(static::class, '\\'), '-'),
             ),
         ];
+    }
+
+    /** @return array<string, array{0: string, 1: bool, 2: bool}> */
+    private static function buildPropTypeMap(): array
+    {
+        $refObj = new ReflectionClass(static::class);
+        $properties = $refObj->getProperties();
+        $propertyTypeMap = [];
+
+        $properties = array_diff_key($properties, array_flip(self::PROPERTIES_TO_IGNORE));
+
+        foreach ($properties as $prop) {
+            $type = self::typeFromProperty($prop, $refObj);
+
+            $propertyTypeMap[$prop->getName()] = [
+                $type->getName(),
+                self::isScalarType($type->getName()),
+                $type->allowsNull(),
+            ];
+        }
+
+        return $propertyTypeMap;
     }
 
     abstract protected function detail(): string;
